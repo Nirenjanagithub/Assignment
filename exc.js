c4e1adbbb7d7252a20d858c84ac82f71f3dab709
Nirenjana <unirenjana@gmail.com>
2024-06-03 00:24:00 +0530

    revised code

--- exc.js
+++ exc.js
@@ -10,24 +10,25 @@ let inputvalue=document.getElementById("input-value").value;
 
 
     const logTime = new Date();
+
     const onTime = new Date();
-    onTime.setHours(21,50,0,0);
+    onTime.setHours(10,30,0,0);
     const tenMinLate = new Date();
-    tenMinLate.setHours(22,40,0,0);
+    tenMinLate.setHours(10,40,0,0);
 
     if(logTime<= onTime){
-        displayText= "Hey "+inputvalue+" ,You are on time.Keepup the gppd work!!";
+        displayText= inputvalue+" ,You are hitting the BULL'S EYE"+`<img  class="img-style"src="bullseye.png" width="4%">`;
 
     }
     else if (logTime <= tenMinLate){
-        displayText= "Hey "+ inputvalue +" ,You are litle bit late."
+        displayText= "Hey "+ inputvalue +" ,You are running on ISLAND " +`<img class="img-style" src="island.png" width="4%">`+"    time!!."
     
     }
     else{
-    displayText=inputvalue+"you are late"
+    displayText="Hey"+ inputvalue+" you are running on TURTLE"+`<img class="img-style"src="turtle.png" width="4%">`+" time!!"
     }
 
-    displayTime.innerText=displayText;
+    displayTime.innerHTML=displayText;
     meetDeet.innerHTML=`<a href="https://meet.google.com/stq-hqgb-zem?hs=224"  target="_blank" class="style"><img src="login-.png" width="4%">Today's Adventure portal</a>`
  
 
